0150526012ab5e0d1ef2f55e0f1f7a80eca8cb41
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-11-27 09:27:40 +0530

    Add Docker setup with Dockerfile, .dockerignore, and .gitignore for environment variables

---
+++ environment-variables/src/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT;
+app.get("/", (req, res) => {
+  res.json({ message: "hello World" });
+});
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`);
+});
