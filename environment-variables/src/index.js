8125a4e580436b357365a2bd192354da1969b9d0
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-11-27 13:05:16 +0530

    Update Dockerfile and .gitignore to include environment variables; modify response message to include APP_NAME

--- environment-variables/src/index.js
+++ environment-variables/src/index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT;
 app.get("/", (req, res) => {
-  res.json({ message: "hello World" });
+  res.json({ message: `hello World from ${process.env.APP_NAME}` });
 });
 
 app.listen(port, () => {
