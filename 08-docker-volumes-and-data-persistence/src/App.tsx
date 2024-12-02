224cf5469aeffec51299f441e67156965f14d23f
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-12-02 11:43:20 +0530

    Add initial project setup with Docker, Vite, React, and TypeScript configuration

---
+++ 08-docker-volumes-and-data-persistence/src/App.tsx
@@ -0,0 +1,15 @@
+import Hero from "./components/Hero.tsx";
+import About from "./components/About.tsx";
+import NavBar from "./components/NavBar.tsx";
+
+function App() {
+    return (
+        <main>
+            <NavBar/>
+            <Hero/>
+            <About/>
+        </main>
+    )
+}
+
+export default App
